c0042d88a215408a07d9ac39a49f0b7920cc8ee9
julychen77 <julychen.tw@gmail.com>
2025-07-20 08:12:59 +0000

    py

---
+++ lesson7/lesson7_2.py
@@ -0,0 +1,8 @@
+print("Hello! python!")
+print("以下是python的基本資料類型(int,float,str,bool)")
+15 #整數,(int)
+15.0 #浮點數,(float)
+"15.0" #字串,(str)
+'15.0' #字串,(str)
+True #Boolen,(bool)
+False #Boolen,(bool)
